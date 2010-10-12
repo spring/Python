f41af677c4dcc102d02e26014657cd1452346d10
Greg Copeland <greg@copelandconsulting.net>
2010-10-12 09:50:40 -0500

    Simple changes which include less, code, reference of constant variable rather than constant value, and reduction to common code path which results in less code and slightly faster execution.

--- src/wrapper.py
+++ src/wrapper.py
@@ -16,25 +16,26 @@ aiClasses = {}	# {teamId:Class}
 ais       = {}	# {teamId : ({event : function}, team)}
 teams     = {}	# {teamId : team }
 
+
 def handleEvent(teamId, topic, data):
 	try:
-		if (topic==1):
+		useTeam(teamId)
+		if topic == PyAI.EVENT_INIT:
 			team = Team(teamId, data["callback"])
 			teams[teamId] = team
 			if aiClasses.has_key(teamId):
-				useTeam(teamId)
 				ais[teamId]={}
 				aiClasses[teamId](team, {"ais":ais})
-				if ais[teamId].has_key(PyAI.EVENT_INIT):
-					useTeam(teamId)
-					ais[teamId][PyAI.EVENT_INIT](data)
+
 			else:
 				return -1
-		elif ais.has_key(teamId) and ais[teamId].has_key(topic):
-			useTeam(teamId)
+
+		if ais.has_key(teamId) and ais[teamId].has_key(topic):
 			ais[teamId][topic](data)
+
 	except:
 		traceback.print_exception(sys.exc_type, sys.exc_value,sys.exc_traceback)
+
 
 def useTeam(teamId):
 	PyAI.team._currentTeam = teams[teamId]
