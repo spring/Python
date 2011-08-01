c2d07364bbf9083e6f451339e7df41123e8ce76d
Andreas Löscher <loesa@hrz.tu-chemnitz.de>
2011-08-01 15:57:06 +0200

    I started a new implementation of the Python AI interface with the use of regular expressions matching the current interface. The old interface was not compatible anymore.
    regex.py:
    	contains all regular expressions to extract the neccesary informations
    	out of the header files

---
+++ builder/regex.py
@@ -0,0 +1,20 @@
+import re
+
+### CATCH Functions
+# Return Type, Function Name, Argumentlist 
+CALLBACK_FUNCTIONS = re.compile("([_ ,a-zA-Z0-9\*]+)\(CALLING_CONV ([\*a-zA-Z_0-9]+)\)\(([^\)]+)\);")
+
+### CATCH Arguments
+# done via str.split(",") no regex needed
+
+
+### CATCH Enums
+# enum name, enum values
+ENUMS = re.compile("enum ([a-zA-Z]+) \{([a-zA-Z0-9=,\n\t _]+)\};")
+
+### CATCH Enum values
+# Name, Value
+ENUM_VALUES = re.compile("([A-Z_]+)[ \t]+=[ \t]+([0-9]+),")
+
+### CATCH Structs
+STRUCTS = re.compile("struct [^ ]+ \{[^\}]+\}; //\$ [A-Z_]+")
